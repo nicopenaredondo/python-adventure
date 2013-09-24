04577f88645fb0c5a1070547985a50e9a05f2473
Nico R. Penaredondo <npenaredondo@gmail.com>
2013-09-24 15:27:25 +0800

    'added_docstring_and_using_sys'

---
+++ docstring.py
@@ -0,0 +1,10 @@
+def printMax(x, y):
+	'''Prints the maximum of two numbers. 
+	The two values must be integers.'''
+	if x > y :
+		print x, 'is maximum'
+	else:
+		print y, 'is maximum'
+
+printMax(3, 5)
+print printMax.__doc__
